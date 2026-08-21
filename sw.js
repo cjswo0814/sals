2092d32092d66006f03336e98e96561e37d77a90
cjswo0814 <cjswo0814@gmail.com>
2026-08-21 20:54:28 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sals-train-v2';
+const CACHE = 'sals-train-v4';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
